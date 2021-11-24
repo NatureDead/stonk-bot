8304e329b5ac14d13cf381e2d651021ad626ee4f
Natured <NatureDead@users.noreply.github.com>
2021-11-24 13:11:09 +0100

    added portable builds

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,5 @@
 mkdir ../publish
 dotnet build ../src/StonkBot.csproj -c Release -r win10-x64 --self-contained false --output ../publish/StonkBot_Windows
+dotnet build ../src/StonkBot.csproj -c Release -r win10-x64 --self-contained true --output ../publish/StonkBot_Windows_Portable
 dotnet build ../src/StonkBot.csproj -c Release -r linux-x64 --self-contained false --output ../publish/StonkBot_Linux
+dotnet build ../src/StonkBot.csproj -c Release -r linux-x64 --self-contained true --output ../publish/StonkBot_Linux_Portable
